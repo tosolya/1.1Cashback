933471b1959d54539420be285eccb35c0cd8356a
Olga Tarasova <bos.olga18@gmail.com>
2021-03-22 16:59:32 +0300

    Revert "Added junit-platform, changed build.gradle"
    This reverts commit fd6a5d8d052054d870a7fa8f8adccae3a6b96877.

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,42 +1,12 @@
 package ru.netology.service;
 
 import org.junit.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @Test
-    public void shouldRemainEqualBoundary4() {
-        int amount = 1000;
-
-        assertEquals(0,service.remain(amount));
-
-    }
-    @Test
-    public void shouldRemainMoreBoundary4() {
-        int amount = 1500;
-
-        assertEquals(500,service.remain(amount));
-
-    }
-    @Test
-    public void shouldRemainAmountEqual04() {
-        int amount = 0;
-
-        assertEquals(1000,service.remain(amount));
-
-    }
-    @Test
-    public void shouldRemainBeforeBoundary4() {
-        int amount = 600;
-
-        assertEquals(400,service.remain(amount));
-
-
-    }
     @Test
     public void shouldRemainEqualBoundary() {
         int amount = 1000;
